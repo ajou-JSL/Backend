bfb1e61e3649d9c1c90699d35d762f9f1f1bfd60
swlee <swo98@naver.com>
2024-10-02 13:31:54 +0900

    🎉 Begin a project
    2024.10.2

---
+++ src/main/java/study/moum/MoumApplication.java
@@ -0,0 +1,13 @@
+package study.moum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/moum/MoumApplicationTests.java
@@ -0,0 +1,13 @@
+package study.moum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
