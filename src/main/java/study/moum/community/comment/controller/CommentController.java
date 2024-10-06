e57e98a9e600fe26ac24bf9e11dadd6117732787
swlee <swo98@naver.com>
2024-10-06 21:19:09 +0900

    ✨ #5 이슈 태그 올라가는지 테스트
    test

--- src/main/java/study/moum/community/comment/controller/CommentController.java
+++ src/main/java/study/moum/community/comment/controller/CommentController.java
@@ -1,4 +1,7 @@
 package study.moum.community.comment.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CommentController {
 }
--- src/main/java/study/moum/community/comment/service/CommentService.java
+++ src/main/java/study/moum/community/comment/service/CommentService.java
@@ -1,4 +1,7 @@
 package study.moum.community.comment.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
 }
