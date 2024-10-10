6a57c6ef92e388fa889c9792f5ac22443e0e2273
swlee <swo98@naver.com>
2024-10-10 20:53:49 +0900

    ✨ [feat] comment - develop settings
    10/10

--- src/main/java/study/moum/community/comment/domain/CommentEntity.java
+++ src/main/java/study/moum/community/comment/domain/CommentEntity.java
@@ -1,4 +1,26 @@
 package study.moum.community.comment.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import study.moum.auth.domain.entity.MemberEntity;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "Comment")
 public class CommentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "author_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MemberEntity author;
+
+    @Column(name = "content")
+    private String content;
 }
