7d352ccdb41ba14805e0775c104ddc578c45e09b
swlee <swo98@naver.com>
2024-10-14 19:34:17 +0900

    ✏️ [fix] signup path fix : /join -> /signup
    .

--- src/main/java/study/moum/auth/controller/SignupController.java
+++ src/main/java/study/moum/auth/controller/SignupController.java
@@ -17,7 +17,7 @@ import study.moum.global.response.ResultResponse;
 public class SignupController {
     private final SignupService signupService;
 
-    @PostMapping("/join")
+    @PostMapping("/signup")
     public ResponseEntity<ResultResponse> signupMember(@Valid @RequestBody MemberDto.Request memberRequestDto) {
 
         signupService.signupMember(memberRequestDto);
