2264f5ab26f09e10d8639267e7c42ae8ec5a60c5
swlee <swo98@naver.com>
2024-10-31 18:51:23 +0900

    temp

--- src/main/java/study/moum/auth/domain/entity/MemberEntity.java
+++ src/main/java/study/moum/auth/domain/entity/MemberEntity.java
@@ -33,7 +33,7 @@ public class MemberEntity {
     private String username;
 
     @Column(name = "password", nullable = false)
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,20}$")
+//    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,20}$")
     private String password;
 
     @Column(name = "profile_description", nullable = true)
--- src/main/java/study/moum/auth/dto/MemberDto.java
+++ src/main/java/study/moum/auth/dto/MemberDto.java
@@ -24,7 +24,7 @@ public class MemberDto {
         private String username;
 
         @NotEmpty @NotNull
-        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,20}$")
+//        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,20}$")
         private String password;
 
         @NotEmpty @NotNull @Email
